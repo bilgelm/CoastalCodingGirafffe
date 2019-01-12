fb7b530c4cb77b3ebf012cbddd018ec12424b9f7
Murat Bilgel <bilgelm@gmail.com>
2019-01-12 11:43:32 -0500

    template fix

--- GIRAFFE/code/workflow.py
+++ GIRAFFE/code/workflow.py
@@ -12,7 +12,7 @@ import nipype.interfaces.fsl as fsl
 my_io_S3DataGrabber = pe.Node(io.S3DataGrabber(outfields=["func, anat"]), name = 'my_io_S3DataGrabber')
 my_io_S3DataGrabber.inputs.bucket = 'openneuro'
 my_io_S3DataGrabber.inputs.sort_filelist = True
-my_io_S3DataGrabber.inputs.template = 'sub-01/anat/sub-01_T1w.nii.gz'
+my_io_S3DataGrabber.inputs.template = 'sub-01/(anat|func)/sub-01_(.*run-0?1_bold|T1w).nii.gz'
 my_io_S3DataGrabber.inputs.anon = True
 my_io_S3DataGrabber.inputs.bucket_path = 'ds000101/ds000101_R2.0.0/uncompressed/'
 my_io_S3DataGrabber.inputs.local_directory = '/tmp'
@@ -33,7 +33,7 @@ analysisflow.connect(my_io_S3DataGrabber, "anat", my_fsl_BET, "in_file")
 analysisflow.connect(my_io_S3DataGrabber, "func", my_fsl_EpiReg, "epi")
 analysisflow.connect(my_fsl_BET, "out_file", my_fsl_EpiReg, "t1_brain")
 analysisflow.connect(my_io_S3DataGrabber, "anat", my_fsl_EpiReg, "t1_head")
-analysisflow.connect(my_fsl_EpiReg, "out_file", my_io_DataSink, "BET_results")
+analysisflow.connect(my_fsl_EpiReg, "out_file", my_io_DataSink, "registered")
 
 #Run the workflow
 plugin = 'MultiProc' #adjust your desired plugin here
